79f58c5fd5110b00f6ec0cc0a9f211667852ddce
Melanie Costello <melaniecostello@Melanies-MacBook-Pro.local>
2017-11-19 18:00:12 -0800

    Melanie updates

--- MC-Explore/hrc_viz_2/js/hrc-viz-2.js
+++ MC-Explore/hrc_viz_2/js/hrc-viz-2.js
@@ -1,28 +1,36 @@
 
-const margin = {top: 40, right: 10, bottom: 10, left: 10},
+var margin = {top: 40, right: 10, bottom: 10, left: 10},
       width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom,
+      height = 540 - margin.top - margin.bottom,
       color = d3.scaleOrdinal().range(d3.schemeCategory20);
     // color = d3.scaleOrdinal().range(d3.schemeSet3);
 
-const treemap = d3.treemap().size([width, height]);
+var treemap = d3.treemap().size([width, height]);
 
-const div = d3.select("body").append("div")
+var div = d3.select("body").append("div")
+    .attr("class", "chart")
     .style("position", "relative")
     .style("width", (width + margin.left + margin.right) + "px")
     .style("height", (height + margin.top + margin.bottom) + "px")
     .style("left", margin.left + "px")
     .style("top", margin.top + "px");
 
-d3.json("data/best_hrc.json", function(error, data) {
+var infowin = d3.select("body").append("g")
+      .attr("class", "infowin");
+
+
+
+
+d3.json("data/best_hrc_19NOV.json", function(error, data) {
   if (error) throw error;
 
-  const root = d3.hierarchy(data, (d) => d.children)
+
+  var root = d3.hierarchy(data, (d) => d.children)
     .sum((d) => d.size);
 
-  const tree = treemap(root);
+  var tree = treemap(root);
 
-  const node = div.datum(root).selectAll(".node")
+  var node = div.datum(root).selectAll(".node")
       .data(tree.leaves())
     .enter().append("div")
       .attr("class", "node")
@@ -31,14 +39,20 @@ d3.json("data/best_hrc.json", function(error, data) {
       .style("width", (d) => Math.max(0, d.x1 - d.x0 - 1) + "px")
       .style("height", (d) => Math.max(0, d.y1 - d.y0  - 1) + "px")
       .style("background", (d) => color(d.parent.data.name))
-      .text((d) => d.data.name);
+      .text((d) => d.data.name)
+      .on("mousemove", function(d){
+            d3.select(".infowin")
+                .text("Sector: " + d.parent.data.name)
+                ;
+        });
+
 
   d3.selectAll("input").on("change", function change() {
-    const value = this.value === "count"
+    var value = this.value === "count"
         ? (d) => { return d.size ? 1 : 0;}
         : (d) => { return d.size; };
 
-    const newRoot = d3.hierarchy(data, (d) => d.children)
+    var newRoot = d3.hierarchy(data, (d) => d.children)
       .sum(value);
 
     node.data(treemap(newRoot).leaves())
